3cb6ecb10b9e0542e9988b37a2b43440b4ab8c6d
sharmita95 <sharmita.shee@viaconteam.com>
2024-08-31 16:54:56 +0530

    search fixation

--- wp-content/themes/law-theme/search.php
+++ wp-content/themes/law-theme/search.php
@@ -1,4 +1,20 @@
-<?php get_header(); ?>
+<?php get_header();
+
+$post_per_page = get_option('posts_per_page');
+$paged = get_query_var( 'paged' ) ? (int) get_query_var( 'paged' ) : 1;
+$args = array(
+    's' => get_search_query(),
+    'post_type' => 'post',
+    'paged'  => $paged,
+    'posts_per_page' => $post_per_page,
+    'order'      =>'DESC',
+    'post_status' => 'publish',
+);
+
+$search_arr = new WP_Query($args);
+$page_count = $search_arr->max_num_pages;
+$post_count = $search_arr->found_posts;
+?>
 
 <section class="lawyers-common-banner-sec">
     <div class="container mx-auto">
@@ -20,8 +36,8 @@
             <div id = "posts-container" class="lawyers-card-grid">
                 
                 <?php
-                if ( have_posts() ) : 
-                    while ( have_posts() ) : the_post();
+                if ( $search_arr->have_posts() ) : 
+                    while ( $search_arr->have_posts() ) : $search_arr->the_post();
 
                         get_template_part('template-parts/blog-card', 'card'); 
 
@@ -32,16 +48,28 @@
         </div>
 
         <!---- Pagination ---->      
-        <div id="pagination-container">
+        <!-- <div id="pagination-container">
             <div class="pagination-wrapper">
-                <?php the_posts_pagination(
+                <?php /*the_posts_pagination(
                     array(
                         'mid_size' => 1,
                         'prev_text' => __( '<<', 'lyi' ),
                         'next_text' => __( '>>', 'lyi' ),
                     )
-                ); ?>
+                );*/ ?>
             </div>
+        </div> -->
+
+        <div id="pagination-container">
+            <?php echo custom_pagination(array(
+                'base'    => get_permalink(),
+                'current' => $paged,
+                'total'   => $page_count,
+                'mid_size' => 2,
+                'end_size' => 1,
+                'prev_text' => '&laquo; prev',
+                'next_text' => 'Next &raquo;'
+            )); ?>
         </div>
 
     </div>
